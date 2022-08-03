42f86453c616ec7c00ab0962441a12b90684170e
originilala <jonathan.dewsaran@gmail.com>
2022-08-03 15:52:00 +0200

    finish assigment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,51 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import logo from '../assets/banana-01.png';
-import { useHistory, Link } from 'react-router-dom';
+import {useHistory, Link} from 'react-router-dom';
+import {AuthContext} from "../AuthContext";
 
 function NavBar() {
-  const history = useHistory();
+    const history = useHistory();
+    const {authState, logOut} = useContext(AuthContext);
+    console.log(authState);
 
-  return (
-    <nav>
-        <Link to="/">
+    return (
+        <nav>
+            <Link to="/">
           <span className="logo-container">
             <img src={logo} alt="logo"/>
             <h3>
               Banana Security
             </h3>
           </span>
-        </Link>
+            </Link>
 
-      <div>
-        <button
-          type="button"
-          onClick={() => history.push('/signin')}
-        >
-          Log in
-        </button>
-        <button
-          type="button"
-          onClick={() => history.push('/signup')}
-        >
-          Registreren
-        </button>
-      </div>
-    </nav>
-  );
+            <div>
+                {authState ?
+                    <button
+                    type="button"
+                    onClick={logOut}
+                >
+                    Log out
+                </button> :
+                <div>
+                    <button
+                        type="button"
+                        onClick={() => {
+                            history.push('/signin');
+                        }}
+                    >
+                        Log in
+                    </button>
+                    <button
+                        type="button"
+                        onClick={() => history.push('/signup')}
+                    >
+                        Registreren
+                    </button>
+                </div>}
+            </div>
+        </nav>
+    );
 }
 
 export default NavBar;
